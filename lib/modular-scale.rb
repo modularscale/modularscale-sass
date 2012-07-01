04d8c96305c35a1694e6667b3b897e2cc9447943
Mason Wendell <mason@zivtech.com>
2012-07-01 02:36:15 -0400

    adding ruby list sorting

--- lib/modular-scale.rb
+++ lib/modular-scale.rb
@@ -1,8 +1,12 @@
 require 'compass'
 Compass::Frameworks.register("modular-scale", :path => "#{File.dirname(__FILE__)}/..")
 
+# Modular Scale Functions
+# module Sass::Script::Functions
+# end
+
 module ModularScale
-  
+
   VERSION = "0.0.6"
   DATE = "2011-09-15"
 

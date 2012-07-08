0a7022a8c32ae051a69019ef1442f4f984076326
Mason Wendell <mason@zivtech.com>
2012-07-08 14:06:49 -0400

    moved ratios to ruby

--- test/config.rb
+++ test/config.rb
@@ -38,6 +38,84 @@ module Sass::Script
 end
 
 module Sass::Script::Functions
+  # Modular Scale
+  def double_octave
+    value = 4 / 1.0
+    Sass::Script::Number.new(value)
+  end
+  def major_twelfth
+    value = 3 / 1.0
+    Sass::Script::Number.new(value)
+  end
+  def major_eleventh
+    value = 8 / 3.0
+    Sass::Script::Number.new(value)
+  end
+  def major_tenth
+    value = 5 / 2.0
+    Sass::Script::Number.new(value)
+  end
+  def octave
+    value = 2 / 1.0
+    Sass::Script::Number.new(value)
+  end
+  def major_seventh
+    value = 15 / 8.0
+    Sass::Script::Number.new(value)
+  end
+  def minor_seventh
+    value = 16 /9.0
+    Sass::Script::Number.new(value)
+  end
+  def major_sixth
+    value = 5 / 3.0
+    Sass::Script::Number.new(value)
+  end
+  def minor_sixth
+    value = 8 / 5.0
+    Sass::Script::Number.new(value)
+  end
+  def fifth
+    value = 3 / 2.0
+    Sass::Script::Number.new(value)
+  end
+  def augmented_forth
+    value = Math.sqrt(2) / 1.0
+    Sass::Script::Number.new(value)
+  end
+  def fourth
+    value = 4 / 3.0
+    Sass::Script::Number.new(value)
+  end
+  def major_third
+    value = 5 / 4.0
+    Sass::Script::Number.new(value)
+  end
+  def minor_third
+    value = 6 / 5.0
+    Sass::Script::Number.new(value)
+  end
+  def major_second
+    value = 9 / 8.0
+    Sass::Script::Number.new(value)
+  end
+  def minor_second
+    value = 16 / 15.0
+    Sass::Script::Number.new(value)
+  end
+  # def modular_scale(multiple, base_size, ratio)
+  #   assert_type multiple, :Number
+  #   if !multiple.int?
+  #     raise ArgumentError.new("Multiple #{multiple} must be an integer")
+  #   end
+  #   puts multiple
+  #   puts base_size
+  #   puts ratio
+
+  #   Sass::Script::Number.new(16, 'px')
+  # end
+  # declare :modular_scale, [:multiple]
+
   # Lists
   def sort_list(list)
     sep = list.separator if list.is_a?(Sass::Script::List)
